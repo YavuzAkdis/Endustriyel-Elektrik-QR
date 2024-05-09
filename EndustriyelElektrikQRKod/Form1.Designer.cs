d4217648cf08e6c7c4432b638ff57bdbf26025d7
Yavuz Akdis <yavuz_akdis@hotmail.com>
2024-05-09 16:12:40 +0300

    QR Code oluşturan uygulama

---
+++ EndustriyelElektrikQRKod/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace EndustriyelElektrikQRKod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.qrCodePictureBox = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(22, 154);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(614, 22);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "QR Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EndustriyelElektrikQRKod.Properties.Resources.Endustriyel_Logo2;
+            this.pictureBox1.Location = new System.Drawing.Point(224, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // qrCodePictureBox
+            // 
+            this.qrCodePictureBox.Location = new System.Drawing.Point(267, 251);
+            this.qrCodePictureBox.Name = "qrCodePictureBox";
+            this.qrCodePictureBox.Size = new System.Drawing.Size(151, 145);
+            this.qrCodePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.qrCodePictureBox.TabIndex = 2;
+            this.qrCodePictureBox.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(664, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.qrCodePictureBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Endüstriyel Elektrik QR Kod";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox qrCodePictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EndustriyelElektrikQRKod/Form1.cs
@@ -0,0 +1,70 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace EndustriyelElektrikQRKod
+{
+    public partial class Form1 : Form
+    {
+        Bitmap qrCodeImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textToEncode = inputTextBox.Text.Trim(); // QR kodu oluşturulacak metni alın
+            if (!string.IsNullOrEmpty(textToEncode))
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator(); // QR kodu oluşturucu nesnesi oluşturun
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(textToEncode, QRCodeGenerator.ECCLevel.Q); // QR kodu verilerini oluşturun
+
+                QRCode qrCode = new QRCode(qrCodeData); // QR kodu oluşturun
+                qrCodeImage = qrCode.GetGraphic(10); // QR kodunun görüntüsünü alın
+
+                qrCodePictureBox.Image = qrCodeImage; // QR kodunu PictureBox kontrolüne atayın
+            }
+            else
+            {
+                MessageBox.Show("Lütfen QR kodu oluşturulacak metni girin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (qrCodeImage != null)
+            {
+                // Kullanıcıya dosya kaydetme iletişim kutusunu göster
+                using (SaveFileDialog saveDialog = new SaveFileDialog())
+                {
+                    saveDialog.Filter = "PNG dosyaları (*.png)|*.png|Tüm dosyalar (*.*)|*.*";
+                    saveDialog.FilterIndex = 1;
+                    saveDialog.FileName = "QRCode.png";
+
+                    if (saveDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Kullanıcının seçtiği yolu kullanarak dosyayı PNG olarak kaydet
+                        qrCodeImage.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+
+                        // Kaydedilen dosyanın yolu hakkında kullanıcıyı bilgilendirin
+                        MessageBox.Show($"QR kodu başarıyla kaydedildi: {saveDialog.FileName}", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Öncelikle bir QR kodu oluşturun.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
